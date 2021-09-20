655fbfa4114454cae77e8b895739933c8640d1ed
blairq <to_zjq_vip@163.com>
2021-09-20 17:57:00 +0800

    add pre-commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link>|
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About组件</router-link>
   </div>
   <router-view />
 </template>
