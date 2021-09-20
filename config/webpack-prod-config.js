5ab95ee98c6d420a81aad36fc205dedf8366c9c3
blairq <to_zjq_vip@163.com>
2021-09-20 17:37:48 +0800

    init commit

---
+++ config/webpack-prod-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mode: "production",
+    devServer: {
+        port: 2777,
+        open: true
+    }
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,6 +15,5 @@ import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
   }
 })
 export default class Home extends Vue { }
+
 </script>
-<style scoped lang="scss">
-</style>
